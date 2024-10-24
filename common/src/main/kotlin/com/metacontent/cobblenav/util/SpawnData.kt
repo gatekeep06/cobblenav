9a8aa64f1a40229af210f74c917655565c371f80
gatekeep06 <mvmetacontent@gmail.com>
2024-10-24 09:55:47 +0300

    collect more spawn data

--- common/src/main/kotlin/com/metacontent/cobblenav/util/SpawnData.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/util/SpawnData.kt
@@ -2,15 +2,16 @@ package com.metacontent.cobblenav.util
 
 import com.cobblemon.mod.common.Cobblemon
 import com.cobblemon.mod.common.api.net.serializers.PoseTypeDataSerializer
+import com.cobblemon.mod.common.api.spawning.condition.AreaTypeSpawningCondition
 import com.cobblemon.mod.common.api.spawning.detail.PokemonSpawnDetail
 import com.cobblemon.mod.common.entity.PoseType
 import com.cobblemon.mod.common.pokemon.RenderablePokemon
 import com.cobblemon.mod.common.registry.BiomeTagCondition
-import com.cobblemon.mod.common.util.cobblemonResource
+import com.cobblemon.mod.common.registry.BlockIdentifierCondition
+import com.cobblemon.mod.common.util.*
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.tags.BiomeTags
 import net.minecraft.tags.TagKey
 import net.minecraft.world.level.biome.Biome
 
@@ -19,17 +20,24 @@ data class SpawnData(
     val spawnChance: Float,
     val encountered: Boolean,
     val biome: ResourceLocation,
+    val additionalConditions: Set<String>,
+    val neededBlocks: Set<ResourceLocation>,
     val pose: PoseType
 ) {
     companion object {
         private const val FLYING_BIOME_CONDITION = "is_sky"
         private const val SWIMMING_CONTEXT_CONDITION = "submerged"
+        private const val CLEAR_KEY = "clear"
+        private const val RAIN_KEY = "rain"
+        private const val THUNDER_KEY = "thunder"
 
         fun decode(buffer: RegistryFriendlyByteBuf): SpawnData = SpawnData(
             RenderablePokemon.loadFromBuffer(buffer),
             buffer.readFloat(),
             buffer.readBoolean(),
             buffer.readResourceLocation(),
+            buffer.readList { it.readString() }.toSet(),
+            buffer.readList { it.readResourceLocation() }.toSet(),
             PoseTypeDataSerializer.read(buffer)
         )
 
@@ -52,6 +60,28 @@ data class SpawnData(
                 .toSet()
             val biome = biomes.firstOrNull { contextBiomeTags.contains(it.tag) }?.tag?.location ?: cobblemonResource("is_overworld")
 
+            val condition = detail.conditions.firstOrNull { cond ->
+                cond.biomes?.any { b ->
+                    b is BiomeTagCondition && b.tag.location == biome
+                } ?: false
+            }
+
+            val additionalConditions = mutableSetOf<String>()
+            val neededBlocks = mutableSetOf<ResourceLocation>()
+            condition?.let {
+                // TODO: check how isThundering works
+                if (condition.isThundering == true) additionalConditions.add(THUNDER_KEY)
+                else if (condition.isRaining == true) additionalConditions.add(RAIN_KEY)
+                if (condition.isRaining == false) additionalConditions.add(CLEAR_KEY)
+                // TODO: moon phases and time ranges
+                if (condition is AreaTypeSpawningCondition) {
+                    condition.neededNearbyBlocks
+                        ?.filterIsInstance<BlockIdentifierCondition>()
+                        ?.map { it.identifier }
+                        ?.let { neededBlocks.addAll(it) }
+                }
+            }
+
             var pose = PoseType.PROFILE
             if (detail.context.name == SWIMMING_CONTEXT_CONDITION) {
                 pose = PoseType.SWIM
@@ -60,7 +90,7 @@ data class SpawnData(
                 pose = PoseType.FLY
             }
 
-            return SpawnData(renderablePokemon, spawnChance, encountered, biome, pose)
+            return SpawnData(renderablePokemon, spawnChance, encountered, biome, additionalConditions, neededBlocks, pose)
         }
     }
 
@@ -69,6 +99,8 @@ data class SpawnData(
         buffer.writeFloat(spawnChance)
         buffer.writeBoolean(encountered)
         buffer.writeResourceLocation(biome)
+        buffer.writeCollection(additionalConditions) { buf, condition -> buf.writeString(condition) }
+        buffer.writeCollection(neededBlocks) { buf, location -> buf.writeResourceLocation(location) }
         PoseTypeDataSerializer.write(buffer, pose)
     }
 }
