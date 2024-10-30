4895ae653f1b7caac5ddde1627cc28388232bfa4
gatekeep06 <mvmetacontent@gmail.com>
2024-10-30 12:38:10 +0300

    add FoundPokemon packet and handler

---
+++ common/src/main/kotlin/com/metacontent/cobblenav/networking/handler/client/FoundPokemonHandler.kt
@@ -0,0 +1,15 @@
+package com.metacontent.cobblenav.networking.handler.client
+
+import com.cobblemon.mod.common.api.net.ClientNetworkPacketHandler
+import com.metacontent.cobblenav.client.gui.screen.FinderScreen
+import com.metacontent.cobblenav.networking.packet.client.FoundPokemonPacket
+import net.minecraft.client.Minecraft
+
+object FoundPokemonHandler : ClientNetworkPacketHandler<FoundPokemonPacket> {
+    override fun handle(packet: FoundPokemonPacket, client: Minecraft) {
+        val screen = client.screen
+        if (screen is FinderScreen) {
+            screen.receiveFoundPokemon(packet.pokemon)
+        }
+    }
+}
