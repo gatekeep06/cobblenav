cf565213541bc917f73cda8b54b06247c3807928
gatekeep06 <mvmetacontent@gmail.com>
2024-10-31 23:46:59 +0300

    add find button, fix opening animation

--- common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/FinderScreen.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/FinderScreen.kt
@@ -7,6 +7,7 @@ import com.cobblemon.mod.common.entity.PoseType
 import com.metacontent.cobblenav.client.gui.util.Timer
 import com.metacontent.cobblenav.client.gui.util.drawPokemon
 import com.metacontent.cobblenav.client.gui.widget.button.IconButton
+import com.metacontent.cobblenav.client.gui.widget.button.TextButton
 import com.metacontent.cobblenav.client.gui.widget.finder.FoundPokemonWidget
 import com.metacontent.cobblenav.networking.packet.server.FindPokemonPacket
 import com.metacontent.cobblenav.util.finder.FoundPokemon
@@ -27,14 +28,20 @@ class FinderScreen(
         const val FADING_DURATION: Float = 5f
         const val POKEBALL_PART_WIDTH: Int = 308
         const val POKEBALL_PART_HEIGHT: Int = 134
+        const val FIND_BUTTON_WIDTH: Int = 112
+        const val FIND_BUTTON_HEIGHT: Int = 32
+        const val FIND_BUTTON_OFFSET: Int = 2
+        const val FIND_BUTTON_TEXT: String = "gui.cobblenav.finder.find_button"
         val POKEBALL_TOP = cobblenavResource("textures/gui/pokeball_screen_top.png")
         val POKEBALL_BOTTOM = cobblenavResource("textures/gui/pokeball_screen_bottom.png")
+        val FIND_BUTTON = cobblenavResource("textures/gui/button/find_button.png")
     }
 
     override val color = FastColor.ARGB32.color(255, 190, 72, 72)
     private var loading = false
     private lateinit var pokemon: FoundPokemon
     private lateinit var foundPokemonWidget: FoundPokemonWidget
+    private lateinit var findButton: TextButton
     private val closingTimer = Timer(CLOSING_DURATION)
     private val fadingTimer = Timer(FADING_DURATION)
     private var pokemonX = 0
@@ -62,6 +69,18 @@ class FinderScreen(
 
         foundPokemonWidget = FoundPokemonWidget(pokemonX, pokemonY, spawnData, pokemon).also { addBlockableWidget(it) }
 
+        findButton = TextButton(
+            pX = screenX + (WIDTH - FIND_BUTTON_WIDTH) / 2,
+            pY = screenY + HEIGHT - HORIZONTAL_BORDER_DEPTH - FIND_BUTTON_HEIGHT - FIND_BUTTON_OFFSET,
+            pWidth = FIND_BUTTON_WIDTH,
+            pHeight = FIND_BUTTON_HEIGHT,
+            disabled = !pokemon.found,
+            texture = FIND_BUTTON,
+            text = Component.translatable(FIND_BUTTON_TEXT),
+            shadow = true,
+            action = {  }
+        ).also { addBlockableWidget(it) }
+
         loading = false
     }
 
@@ -82,6 +101,16 @@ class FinderScreen(
     }
 
     override fun renderOnFrontLayer(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
+
+    }
+
+    override fun render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
+        if (closingTimer.isOver()) {
+            super.render(guiGraphics, mouseX, mouseY, delta)
+        }
+        else {
+            previousScreen?.render(guiGraphics, mouseX, mouseY, delta)
+        }
         if (!fadingTimer.isOver()) {
             renderPokeballAnimation(guiGraphics, mouseX, mouseY, delta)
             closingTimer.tick(delta)
@@ -93,8 +122,13 @@ class FinderScreen(
     private fun renderPokeballAnimation(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
         val poseStack = guiGraphics.pose()
 
+        guiGraphics.enableScissor(
+            screenX + VERTICAL_BORDER_DEPTH,
+            screenY + HORIZONTAL_BORDER_DEPTH,
+            screenX + VERTICAL_BORDER_DEPTH + SCREEN_WIDTH,
+            screenY + HORIZONTAL_BORDER_DEPTH + SCREEN_HEIGHT,
+        )
         poseStack.pushPose()
-        if (!closingTimer.isOver()) previousScreen?.render(guiGraphics, mouseX, mouseY, delta)
         poseStack.translate(0f, 0f, 400f)
         blitk(
             matrixStack = poseStack,
@@ -115,6 +149,7 @@ class FinderScreen(
             alpha = 1f - fadingTimer.getProgress()
         )
         poseStack.popPose()
+        guiGraphics.disableScissor()
     }
 
     private fun findPokemon() {
