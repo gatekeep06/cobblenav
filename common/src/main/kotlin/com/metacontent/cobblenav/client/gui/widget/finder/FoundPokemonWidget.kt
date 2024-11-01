f0e32ce31a96cf3a04f501b59bd6778e533caa8e
gatekeep06 <mvmetacontent@gmail.com>
2024-11-01 17:54:48 +0300

    even more decorations

--- common/src/main/kotlin/com/metacontent/cobblenav/client/gui/widget/finder/FoundPokemonWidget.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/widget/finder/FoundPokemonWidget.kt
@@ -1,12 +1,15 @@
 package com.metacontent.cobblenav.client.gui.widget.finder
 
+import com.cobblemon.mod.common.api.gui.blitk
 import com.cobblemon.mod.common.client.gui.summary.widgets.SoundlessWidget
 import com.cobblemon.mod.common.client.render.models.blockbench.FloatingState
 import com.cobblemon.mod.common.entity.PoseType
 import com.cobblemon.mod.common.util.math.fromEulerXYZDegrees
 import com.metacontent.cobblenav.client.gui.util.Timer
+import com.metacontent.cobblenav.client.gui.util.drawBlurredArea
 import com.metacontent.cobblenav.client.gui.util.drawPokemon
 import com.metacontent.cobblenav.util.SpawnData
+import com.metacontent.cobblenav.util.cobblenavResource
 import com.metacontent.cobblenav.util.finder.FoundPokemon
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.network.chat.Component
@@ -25,7 +28,10 @@ class FoundPokemonWidget(
         const val SCALE: Float = 40f
         const val OPENING: Float = 12f
         const val LOOP: Float = 5000f
+        const val BAR_LENGTH: Int = 12
         const val BARS: Int = 36
+        val DECORATIONS_1 = cobblenavResource("textures/gui/finder/finder_decorations_1.png")
+        val DECORATIONS_2 = cobblenavResource("textures/gui/finder/finder_decorations_2.png")
     }
 
     private val state = FloatingState()
@@ -38,13 +44,34 @@ class FoundPokemonWidget(
         val poseStack = guiGraphics.pose()
 
         poseStack.pushPose()
-        poseStack.rotateAround(Quaternionf().fromEulerXYZDegrees(Vector3f(0f, 0f, 360f * loopTimer.getProgress())), x.toFloat(), y.toFloat(), 0f)
+        poseStack.rotateAround(Quaternionf().fromEulerXYZDegrees(Vector3f(0f, 0f, 360f * (loopTimer.getProgress() + (1f - openingTimer.getProgress())))), x.toFloat(), y.toFloat(), 0f)
         for (barIndex in 0 until (BARS * openingTimer.getProgress()).toInt()) {
             poseStack.pushPose()
             poseStack.rotateAround(Quaternionf().fromEulerXYZDegrees(Vector3f(0f, 0f, barIndex * 360f / BARS)), x.toFloat(), y.toFloat(), 0f)
-            guiGraphics.fill(x - 2, y + RADIUS, x + 2, y + RADIUS + 12, FastColor.ARGB32.color(128, 0, 0, 0))
+            guiGraphics.fill(x - 2, y + RADIUS, x + 2, y + RADIUS + BAR_LENGTH, FastColor.ARGB32.color(128, 0, 0, 0))
             poseStack.popPose()
         }
+        poseStack.popPose()
+        poseStack.pushPose()
+        poseStack.rotateAround(Quaternionf().fromEulerXYZDegrees(Vector3f(0f, 0f, -360f * (loopTimer.getProgress() + (1f - openingTimer.getProgress())))), x.toFloat(), y.toFloat(), 0f)
+        blitk(
+            matrixStack = poseStack,
+            texture = DECORATIONS_1,
+            x = x - RADIUS - BAR_LENGTH - 32,
+            y = y - RADIUS - BAR_LENGTH - 32,
+            width = 2 * (RADIUS + BAR_LENGTH + 32),
+            height = 2 * (RADIUS + BAR_LENGTH + 32),
+            alpha = 0.8f
+        )
+        blitk(
+            matrixStack = poseStack,
+            texture = DECORATIONS_2,
+            x = x - RADIUS + 4,
+            y = y - RADIUS + 4,
+            width = 2 * (RADIUS - 4),
+            height = 2 * (RADIUS - 4),
+            alpha = 0.8f
+        )
         poseStack.popPose()
 
         drawPokemon(
