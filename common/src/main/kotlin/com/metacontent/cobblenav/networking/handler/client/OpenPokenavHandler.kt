41f572d72084cb639638a9eb8467d9bff095c8e2
gatekeep06 <mvmetacontent@gmail.com>
2025-01-04 23:39:06 +0300

    change landing screen

--- common/src/main/kotlin/com/metacontent/cobblenav/networking/handler/client/OpenPokenavHandler.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/networking/handler/client/OpenPokenavHandler.kt
@@ -2,6 +2,7 @@ package com.metacontent.cobblenav.networking.handler.client
 
 import com.cobblemon.mod.common.api.net.ClientNetworkPacketHandler
 import com.metacontent.cobblenav.client.CobblenavClient
+import com.metacontent.cobblenav.client.gui.screen.LocationScreen
 import com.metacontent.cobblenav.client.gui.screen.MainScreen
 import com.metacontent.cobblenav.networking.packet.client.OpenPokenavPacket
 import net.minecraft.client.Minecraft
@@ -12,7 +13,7 @@ object OpenPokenavHandler : ClientNetworkPacketHandler<OpenPokenavPacket> {
             CobblenavClient.trackArrowOverlay.tracking = false
         }
         else {
-            client.setScreen(MainScreen(packet.os, makeOpeningSound = true, animateOpening = true))
+            client.setScreen(LocationScreen(packet.os, makeOpeningSound = true, animateOpening = true))
         }
     }
 }
