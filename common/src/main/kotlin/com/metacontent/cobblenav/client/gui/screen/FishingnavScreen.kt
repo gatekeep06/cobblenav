3342a25efe83ee5836705867bd0bdde1af924708
gatekeep06 <mvmetacontent@gmail.com>
2025-03-01 18:22:10 +0300

    requesting and receiving

--- common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/FishingnavScreen.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/FishingnavScreen.kt
@@ -1,15 +1,38 @@
 package com.metacontent.cobblenav.client.gui.screen
 
+import com.metacontent.cobblenav.networking.packet.server.RequestFishingMapPacket
+import com.metacontent.cobblenav.networking.packet.server.RequestFishingnavScreenInitDataPacket
 import com.metacontent.cobblenav.os.PokenavOS
+import com.metacontent.cobblenav.spawndata.SpawnData
+import com.metacontent.cobblenav.util.WeightedBucket
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.util.FastColor
+import net.minecraft.world.item.ItemStack
 
 class FishingnavScreen(
     os: PokenavOS
 ) : PokenavScreen(os, true, true, Component.literal("Fishing")) {
     override val color = FastColor.ARGB32.color(255, 117, 230, 218)
 
+    lateinit var buckets: List<WeightedBucket>
+
     override fun initScreen() {
+        RequestFishingnavScreenInitDataPacket().sendToServer()
+    }
+
+    fun receiveInitData(
+        buckets: List<WeightedBucket>,
+        applyBuckets: Boolean,
+        pokeBall: ResourceLocation,
+        lineColor: String,
+        baitItem: ItemStack
+    ) {
+        this.buckets = buckets
+        RequestFishingMapPacket().sendToServer()
+    }
+
+    fun receiveFishingMap(fishingMap: Map<String, List<SpawnData>>) {
 
     }
 }
--- common/src/main/kotlin/com/metacontent/cobblenav/networking/handler/client/FishingMapHandler.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/networking/handler/client/FishingMapHandler.kt
@@ -1,16 +1,12 @@
 package com.metacontent.cobblenav.networking.handler.client
 
 import com.cobblemon.mod.common.api.net.ClientNetworkPacketHandler
-import com.cobblemon.mod.common.api.text.bold
+import com.metacontent.cobblenav.client.gui.screen.FishingnavScreen
 import com.metacontent.cobblenav.networking.packet.client.FishingMapPacket
 import net.minecraft.client.Minecraft
-import net.minecraft.network.chat.Component
 
 object FishingMapHandler : ClientNetworkPacketHandler<FishingMapPacket> {
     override fun handle(packet: FishingMapPacket, client: Minecraft) {
-        packet.fishingMap.forEach { (t, u) ->
-            client.player?.sendSystemMessage(Component.literal(t).bold())
-            client.player?.sendSystemMessage(Component.literal(u.joinToString { it.renderable.species.name + ": " + it.spawnChance }))
-        }
+        (client.screen as? FishingnavScreen)?.receiveFishingMap(packet.fishingMap)
     }
 }
