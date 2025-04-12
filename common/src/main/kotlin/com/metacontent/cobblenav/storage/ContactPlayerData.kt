a711ba23ed4d19ec0a29c160c1a72a40faebac28
gatekeep06 <mvmetacontent@gmail.com>
2025-04-12 21:29:32 +0300

    i hate codecs p.4 : endgame

--- common/src/main/kotlin/com/metacontent/cobblenav/storage/ContactPlayerData.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/storage/ContactPlayerData.kt
@@ -5,12 +5,26 @@ import com.cobblemon.mod.common.api.storage.player.client.ClientInstancedPlayerD
 import com.metacontent.cobblenav.api.contact.ContactID
 import com.metacontent.cobblenav.api.contact.PokenavContact
 import com.metacontent.cobblenav.storage.client.ClientContactPlayerData
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.PrimitiveCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import java.util.*
 
 data class ContactPlayerData(
     override val uuid: UUID,
     val contacts: MutableMap<ContactID, PokenavContact>
 ) : InstancedPlayerData {
+    companion object {
+        val CODEC = RecordCodecBuilder.create<ContactPlayerData> { instance ->
+            instance.group(
+                PrimitiveCodec.STRING.fieldOf("uuid").forGetter { it.uuid.toString() },
+                Codec.unboundedMap(ContactID.CODEC, PokenavContact.CODEC).fieldOf("contacts").forGetter { it.contacts }
+            ).apply(instance) { uuid, contacts ->
+                ContactPlayerData(UUID.fromString(uuid), contacts)
+            }
+        }
+    }
+
     override fun toClientData(): ClientInstancedPlayerData {
         return ClientContactPlayerData(contacts.values.map { it.toClientContact() }.toMutableList())
     }
