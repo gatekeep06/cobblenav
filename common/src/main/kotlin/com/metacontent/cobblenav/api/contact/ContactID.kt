2e05af85f874c2a3fda5cde41407f36e05cff129
gatekeep06 <mvmetacontent@gmail.com>
2025-04-12 21:11:39 +0300

    i hate codecs p.1

--- common/src/main/kotlin/com/metacontent/cobblenav/api/contact/ContactID.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/contact/ContactID.kt
@@ -2,6 +2,8 @@ package com.metacontent.cobblenav.api.contact
 
 import com.cobblemon.mod.common.api.net.Encodable
 import com.metacontent.cobblenav.util.cobblenavResource
+import com.mojang.serialization.codecs.PrimitiveCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.resources.ResourceLocation
 import java.util.UUID
@@ -11,6 +13,15 @@ data class ContactID(
     val type: ResourceLocation = cobblenavResource("player")
 ) : Encodable {
     companion object {
+        val CODEC = RecordCodecBuilder.create<ContactID> { instance ->
+            instance.group(
+                PrimitiveCodec.STRING.fieldOf("uuid").forGetter { it.uuid.toString() },
+                ResourceLocation.CODEC.fieldOf("type").forGetter { it.type }
+            ).apply(instance) { uuid, type ->
+                ContactID(UUID.fromString(uuid), type)
+            }
+        }
+
         fun decode(buffer: RegistryFriendlyByteBuf) = ContactID(
             uuid = buffer.readUUID(),
             type = buffer.readResourceLocation()
