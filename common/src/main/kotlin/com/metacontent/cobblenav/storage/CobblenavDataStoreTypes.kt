29153da1249cd393e2f6d1541f70b684bf565d0a
gatekeep06 <mvmetacontent@gmail.com>
2025-04-12 17:03:17 +0300

    refactor store type registration

--- common/src/main/kotlin/com/metacontent/cobblenav/storage/CobblenavDataStoreTypes.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/storage/CobblenavDataStoreTypes.kt
@@ -1,26 +1,21 @@
 package com.metacontent.cobblenav.storage
 
 import com.cobblemon.mod.common.api.storage.player.PlayerInstancedDataStoreType
-import com.cobblemon.mod.common.api.storage.player.PlayerInstancedDataStoreTypes
+import com.cobblemon.mod.common.api.storage.player.PlayerInstancedDataStoreTypes.register
 import com.metacontent.cobblenav.util.cobblenavResource
 
 object CobblenavDataStoreTypes {
-    val PROFILES = PlayerInstancedDataStoreType(
+    val PROFILES = register(PlayerInstancedDataStoreType(
         id = cobblenavResource("profile"),
         decoder = ClientProfilePlayerData::decode,
         afterDecodeAction = ClientProfilePlayerData::afterDecode,
         incrementalAfterDecodeAction = ClientProfilePlayerData::incrementalAfterDecode
-    )
+    ))
 
-    val CONTACTS = PlayerInstancedDataStoreType(
+    val CONTACTS = register(PlayerInstancedDataStoreType(
         id = cobblenavResource("contact_data"),
         decoder = ClientContactPlayerData::decode,
         afterDecodeAction = ClientContactPlayerData::afterDecode,
         incrementalAfterDecodeAction = ClientContactPlayerData::incrementalAfterDecode
-    )
-
-    fun register() {
-        PlayerInstancedDataStoreTypes.register(PROFILES)
-        PlayerInstancedDataStoreTypes.register(CONTACTS)
-    }
+    ))
 }
