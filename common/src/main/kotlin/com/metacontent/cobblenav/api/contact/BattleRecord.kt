4b6443f9902a3ed8204b670f17e3f170143d3fad
gatekeep06 <mvmetacontent@gmail.com>
2025-04-12 21:26:45 +0300

    i hate codecs p.2

--- common/src/main/kotlin/com/metacontent/cobblenav/api/contact/BattleRecord.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/contact/BattleRecord.kt
@@ -3,15 +3,29 @@ package com.metacontent.cobblenav.api.contact
 import com.cobblemon.mod.common.api.net.Encodable
 import com.cobblemon.mod.common.util.readString
 import com.cobblemon.mod.common.util.writeString
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.ListCodec
+import com.mojang.serialization.codecs.PrimitiveCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.network.RegistryFriendlyByteBuf
 import java.time.Instant
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 data class BattleRecord(
     val participants: Map<String, List<PartyMember>>,
     val date: Date = Date.from(Instant.now())
 ) : Encodable {
     companion object {
+        val CODEC: Codec<BattleRecord> = RecordCodecBuilder.create<BattleRecord> { instance ->
+            instance.group(
+                Codec.unboundedMap(PrimitiveCodec.STRING, ListCodec(PartyMember.CODEC, 0, 512)).fieldOf("participants").forGetter { it.participants },
+                PrimitiveCodec.LONG.fieldOf("date").forGetter { it.date.time }
+            ).apply(instance) { participants, date ->
+                BattleRecord(participants, Date(date))
+            }
+        }
+
         fun decode(buffer: RegistryFriendlyByteBuf) = BattleRecord(
             participants = buffer.readMap(
                 { it.readString() },
@@ -37,6 +51,16 @@ data class PartyMember(
     val level: Int
 ) : Encodable {
     companion object {
+        val CODEC: Codec<PartyMember> = RecordCodecBuilder.create<PartyMember> { instance ->
+            instance.group(
+                PrimitiveCodec.STRING.optionalFieldOf("nickname").forGetter { Optional.ofNullable(it.nickname) },
+                PrimitiveCodec.STRING.fieldOf("pokemon").forGetter { it.pokemon },
+                PrimitiveCodec.INT.fieldOf("level").forGetter { it.level }
+            ).apply(instance) { nickname, pokemon, level ->
+                PartyMember(nickname.getOrNull(), pokemon, level)
+            }
+        }
+
         fun decode(buffer: RegistryFriendlyByteBuf) = PartyMember(
             nickname = buffer.readNullable { it.readString() },
             pokemon = buffer.readString(),
