5ebd73922d8e6092ece9c3f62ba6db7de5856e11
gatekeep06 <mvmetacontent@gmail.com>
2025-06-22 19:20:10 +0300

    add npc profile registry

---
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/contact/npc/NPCProfile.kt
@@ -0,0 +1,12 @@
+package com.metacontent.cobblenav.api.contact.npc
+
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties
+import net.minecraft.resources.ResourceLocation
+
+data class NPCProfile(
+    val id: ResourceLocation,
+    val name: String?,
+    val title: ResourceLocation?,
+    val partnerPokemon: PokemonProperties?,
+    val commonForAllEntities: Boolean
+)
