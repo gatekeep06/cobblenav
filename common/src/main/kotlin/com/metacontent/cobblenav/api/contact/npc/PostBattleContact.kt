1fc6f10f5ce19bfe13fd840a8b1c5e4164766f75
gatekeep06 <mvmetacontent@gmail.com>
2025-06-29 13:58:06 +0300

    add post-battle contact providers

---
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/contact/npc/PostBattleContact.kt
@@ -0,0 +1,40 @@
+package com.metacontent.cobblenav.api.contact.npc
+
+import com.cobblemon.mod.common.api.dialogue.Dialogue
+import com.cobblemon.mod.common.api.dialogue.DialogueManager
+import com.cobblemon.mod.common.api.dialogue.Dialogues
+import com.cobblemon.mod.common.entity.npc.NPCEntity
+import com.metacontent.cobblenav.api.contact.PokenavContact
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.server.level.ServerPlayer
+
+interface PostBattleContactProvider {
+    fun canShare(): Boolean
+
+    fun provide(player: ServerPlayer, entity: NPCEntity, contact: PokenavContact): PokenavContact?
+}
+
+class NoContactProvider : PostBattleContactProvider {
+    override fun canShare() = false
+
+    override fun provide(player: ServerPlayer, entity: NPCEntity, contact: PokenavContact): PokenavContact? = null
+}
+
+class ImmediateContactProvider : PostBattleContactProvider {
+    override fun canShare() = true
+
+    override fun provide(player: ServerPlayer, entity: NPCEntity, contact: PokenavContact): PokenavContact = contact
+}
+
+class DialogueContactProvider(
+    val dialogueId: ResourceLocation
+) : PostBattleContactProvider {
+    val dialogue: Dialogue? by lazy { Dialogues.dialogues[dialogueId] }
+
+    override fun canShare() = dialogue != null
+
+    override fun provide(player: ServerPlayer, entity: NPCEntity, contact: PokenavContact): PokenavContact? {
+        dialogue?.let { DialogueManager.startDialogue(player, entity, it) }
+        return null
+    }
+}
---
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/contact/npc/PostBattleContactProviderAdapter.kt
@@ -0,0 +1,33 @@
+package com.metacontent.cobblenav.api.contact.npc
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import net.minecraft.resources.ResourceLocation
+import java.lang.reflect.Type
+
+object PostBattleContactProviderAdapter : JsonDeserializer<PostBattleContactProvider> {
+    override fun deserialize(
+        json: JsonElement,
+        type: Type,
+        context: JsonDeserializationContext
+    ): PostBattleContactProvider {
+        return when (json) {
+            is JsonPrimitive -> if (json.asBoolean) ImmediateContactProvider() else NoContactProvider()
+            else -> {
+                val jsonObject = json.asJsonObject
+                return when (jsonObject.get("type")?.asString) {
+                    "dialogue" -> DialogueContactProvider(
+                        context.deserialize(
+                            jsonObject.get("dialogue"),
+                            ResourceLocation::class.java
+                        )
+                    )
+
+                    else -> NoContactProvider()
+                }
+            }
+        }
+    }
+}
