848de5f49049f858b84457ac5019379b7c2f55e5
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2025-11-18 19:12:47 +0300

    change values type to component list

--- common/src/main/kotlin/com/metacontent/cobblenav/spawndata/ConditionData.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/spawndata/ConditionData.kt
@@ -3,19 +3,21 @@ package com.metacontent.cobblenav.spawndata
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.ComponentSerialization
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 
 data class ConditionData(
     val condition: String,
-    val values: Set<String>
+    val values: List<Component>
 ) {
     companion object {
         val CODEC: Codec<ConditionData> = RecordCodecBuilder.create { instance ->
             instance.group(
                 Codec.STRING.fieldOf("condition").forGetter(ConditionData::condition),
-                Codec.STRING.listOf().fieldOf("values").forGetter { (_, values) -> values.toList() }
-            ).apply(instance) { condition, values -> ConditionData(condition, values.toSet()) }
+                ComponentSerialization.CODEC.listOf().fieldOf("values").forGetter(ConditionData::values)
+            ).apply(instance) { condition, values -> ConditionData(condition, values) }
         }
 
         val BUFF_CODEC: StreamCodec<RegistryFriendlyByteBuf, ConditionData> = ByteBufCodecs.fromCodecWithRegistries(CODEC)
