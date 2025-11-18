7f41cc91143d9cfc39472e464cb5fc2621107995
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2025-11-18 19:24:58 +0300

    update biome collector

--- common/src/main/kotlin/com/metacontent/cobblenav/spawndata/collector/general/BiomeCollector.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/spawndata/collector/general/BiomeCollector.kt
@@ -1,34 +1,24 @@
 package com.metacontent.cobblenav.spawndata.collector.general
 
 import com.cobblemon.mod.common.api.spawning.condition.SpawningCondition
-import com.cobblemon.mod.common.api.spawning.position.SpawnablePosition
-import com.metacontent.cobblenav.Cobblenav
-import com.metacontent.cobblenav.api.platform.SpawnDataContext
-import com.metacontent.cobblenav.util.cobblenavResource
+import com.cobblemon.mod.common.api.spawning.detail.SpawnDetail
+import com.metacontent.cobblenav.api.platform.BiomePlatformContext
+import com.metacontent.cobblenav.spawndata.ConditionData
 import com.metacontent.cobblenav.util.toResourceLocation
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.MutableComponent
 import net.minecraft.server.level.ServerPlayer
-import kotlin.jvm.optionals.getOrNull
 
 class BiomeCollector : GeneralConditionCollector() {
     override val configName = "biomes"
 
     override fun collect(
+        detail: SpawnDetail,
         condition: SpawningCondition<*>,
-        spawnablePositions: List<SpawnablePosition>,
         player: ServerPlayer,
-        builder: SpawnDataContext.Builder
-    ): MutableComponent? {
-        val biomes = condition.biomes
-            ?.mapNotNull { it.toResourceLocation() }
-            ?.toSet()
-        val habitat = Component.translatable("gui.cobblenav.spawn_data.habitat")
-        biomes?.let { set ->
-            builder.biomes = set
-            set.forEach { habitat.append(Component.translatable(it.toLanguageKey("biome")))  }
-        }
-        if (habitat.siblings.isEmpty()) habitat.append(cobblenavResource("any").toLanguageKey("biome"))
-        return habitat
+        builder: BiomePlatformContext.Builder?
+    ): ConditionData? {
+        val biomes = condition.biomes?.mapNotNull { it.toResourceLocation() }?.toSet() ?: return null
+        builder?.biomes = biomes
+        return ConditionData("habitat", biomes.map { Component.translatable(it.toLanguageKey("biome")) })
     }
 }
