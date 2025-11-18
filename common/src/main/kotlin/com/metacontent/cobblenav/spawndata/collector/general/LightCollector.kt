30eead17678c58ca1d4b06b53ca3ebfaf6612d33
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2025-11-18 19:26:38 +0300

    update light collector

--- common/src/main/kotlin/com/metacontent/cobblenav/spawndata/collector/general/LightCollector.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/spawndata/collector/general/LightCollector.kt
@@ -1,24 +1,23 @@
 package com.metacontent.cobblenav.spawndata.collector.general
 
 import com.cobblemon.mod.common.api.spawning.condition.SpawningCondition
-import com.cobblemon.mod.common.api.spawning.position.SpawnablePosition
-import com.metacontent.cobblenav.api.platform.SpawnDataContext
+import com.cobblemon.mod.common.api.spawning.detail.SpawnDetail
+import com.metacontent.cobblenav.api.platform.BiomePlatformContext
+import com.metacontent.cobblenav.spawndata.ConditionData
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.MutableComponent
 import net.minecraft.server.level.ServerPlayer
 
 class LightCollector : GeneralConditionCollector() {
     override val configName = "light"
 
     override fun collect(
+        detail: SpawnDetail,
         condition: SpawningCondition<*>,
-        spawnablePositions: List<SpawnablePosition>,
         player: ServerPlayer,
-        builder: SpawnDataContext.Builder
-    ): MutableComponent? {
-        formatValueRange(condition.minLight, condition.maxLight)?.let {
-            return Component.translatable("gui.cobblenav.spawn_data.light", it)
+        builder: BiomePlatformContext.Builder?
+    ): ConditionData? {
+        return formatValueRange(condition.minLight, condition.maxLight)?.let {
+            ConditionData("light", listOf(Component.translatable("gui.cobblenav.spawn_data.light", it)))
         }
-        return null
     }
 }
