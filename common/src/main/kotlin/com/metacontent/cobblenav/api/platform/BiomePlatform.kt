4674d5e5469ba4363b93c59641d7704461fa6bb6
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2026-03-25 22:26:53 +0300

    raplace platform conditions with spawnable conditions

--- common/src/main/kotlin/com/metacontent/cobblenav/api/platform/BiomePlatform.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/api/platform/BiomePlatform.kt
@@ -1,9 +1,16 @@
 package com.metacontent.cobblenav.api.platform
 
+import com.cobblemon.mod.common.api.spawning.condition.SpawningCondition
+import com.cobblemon.mod.common.api.spawning.position.SpawnablePosition
 import net.minecraft.resources.ResourceLocation
 
 data class BiomePlatform(
     val id: ResourceLocation,
-    val conditions: HashSet<PlatformCondition>,
-    val anticonditions: HashSet<PlatformCondition>?
-)
+    val conditions: List<SpawningCondition<*>>,
+    val anticonditions: List<SpawningCondition<*>>?
+) {
+    fun fits(spawnablePosition: SpawnablePosition): Boolean {
+        return conditions.any { it.isSatisfiedBy(spawnablePosition) } &&
+                anticonditions?.none { it.isSatisfiedBy(spawnablePosition) } != false
+    }
+}
