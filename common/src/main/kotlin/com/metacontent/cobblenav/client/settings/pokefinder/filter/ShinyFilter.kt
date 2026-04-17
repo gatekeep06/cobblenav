f15c4011cb58345832fc400e0c1cbef030b58e59
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2026-04-17 17:52:50 +0300

    Update ShinyFilter.kt

--- common/src/main/kotlin/com/metacontent/cobblenav/client/settings/pokefinder/filter/ShinyFilter.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/settings/pokefinder/filter/ShinyFilter.kt
@@ -2,12 +2,14 @@ package com.metacontent.cobblenav.client.settings.pokefinder.filter
 
 import com.cobblemon.mod.common.pokemon.Pokemon
 
-class ShinyFilter : RadarFilter {
+class ShinyFilter(
+    val enabled: Boolean = false
+) : RadarFilter {
     companion object {
         const val TYPE = "shiny"
     }
 
     override val type = TYPE
 
-    override fun test(pokemon: Pokemon): Boolean = pokemon.shiny
+    override fun test(pokemon: Pokemon): Boolean = !enabled || pokemon.shiny
 }
