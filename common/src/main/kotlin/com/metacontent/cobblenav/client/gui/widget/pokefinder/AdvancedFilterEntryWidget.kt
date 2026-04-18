3354e9617201e80236aabfdc7bae3b45717d5c0a
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2026-04-18 14:41:38 +0300

    add advanced entry widget

---
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/widget/pokefinder/AdvancedFilterEntryWidget.kt
@@ -0,0 +1,44 @@
+package com.metacontent.cobblenav.client.gui.widget.pokefinder
+
+import com.metacontent.cobblenav.client.gui.screen.pokefinder.AdvancedPokefinderScreen
+import com.metacontent.cobblenav.client.gui.util.gui
+import com.metacontent.cobblenav.client.gui.widget.button.IconButton
+import com.metacontent.cobblenav.client.settings.pokefinder.filter.RadarFilter
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.resources.ResourceLocation
+
+class AdvancedFilterEntryWidget(
+    filter: RadarFilter,
+    widget: AbstractWidget,
+    icon: ResourceLocation,
+    parent: AdvancedPokefinderScreen
+) : FilterEntryWidget(filter, widget, icon) {
+    companion object {
+        val REMOVE = gui("pokefinder/remove")
+    }
+
+    val removeButton = IconButton(
+        pX = x,
+        pY = y + (HEIGHT - REMOVE_SIZE) / 2,
+        pWidth = REMOVE_SIZE,
+        pHeight = REMOVE_SIZE,
+        action = { parent.removeFilterListEntry(this) },
+        texture = REMOVE
+    ).also { addWidget(it) }
+
+    override fun renderWidget(guiGraphics: GuiGraphics, i: Int, j: Int, f: Float) {
+        removeButton.render(guiGraphics, i, j, f)
+        super.renderWidget(guiGraphics, i, j, f)
+    }
+
+    override fun setX(i: Int) {
+        removeButton.x += i - x
+        super.setX(i)
+    }
+
+    override fun setY(i: Int) {
+        removeButton.y += i - y
+        super.setY(i)
+    }
+}
--- common/src/main/kotlin/com/metacontent/cobblenav/client/gui/widget/pokefinder/FilterEntryWidget.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/widget/pokefinder/FilterEntryWidget.kt
@@ -2,9 +2,7 @@ package com.metacontent.cobblenav.client.gui.widget.pokefinder
 
 import com.cobblemon.mod.common.api.gui.blitk
 import com.cobblemon.mod.common.client.gui.summary.widgets.SoundlessWidget
-import com.metacontent.cobblenav.client.gui.screen.pokefinder.AdvancedPokefinderScreen
 import com.metacontent.cobblenav.client.gui.util.gui
-import com.metacontent.cobblenav.client.gui.widget.button.IconButton
 import com.metacontent.cobblenav.client.settings.pokefinder.filter.RadarFilter
 import com.metacontent.cobblenav.client.settings.pokefinder.type.RadarFilterType
 import net.minecraft.client.gui.GuiGraphics
@@ -15,25 +13,14 @@ import net.minecraft.resources.ResourceLocation
 open class FilterEntryWidget(
     val filter: RadarFilter,
     val widget: AbstractWidget,
-    val icon: ResourceLocation,
-    parent: AdvancedPokefinderScreen
+    val icon: ResourceLocation
 ) : SoundlessWidget(0, 0, WIDTH, HEIGHT, Component.empty()) {
     companion object {
         const val WIDTH = 238
         const val HEIGHT = 26
         const val REMOVE_SIZE = 16
         val ICON_BACKGROUND = gui("pokefinder/icon_background")
-        val REMOVE = gui("pokefinder/remove")
     }
-
-    val removeButton = IconButton(
-        pX = x,
-        pY = y + (HEIGHT - REMOVE_SIZE) / 2,
-        pWidth = REMOVE_SIZE,
-        pHeight = REMOVE_SIZE,
-        action = { parent.removeFilterListEntry(this) },
-        texture = REMOVE
-    ).also { addWidget(it) }
 
     init {
         widget.x += REMOVE_SIZE + 4
@@ -42,8 +29,6 @@ open class FilterEntryWidget(
 
     override fun renderWidget(guiGraphics: GuiGraphics, i: Int, j: Int, f: Float) {
         val poseStack = guiGraphics.pose()
-
-        removeButton.render(guiGraphics, i, j, f)
 
         widget.render(guiGraphics, i, j, f)
 
@@ -67,13 +52,11 @@ open class FilterEntryWidget(
 
     override fun setX(i: Int) {
         widget.x += i - x
-        removeButton.x += i - x
         super.setX(i)
     }
 
     override fun setY(i: Int) {
         widget.y += i - y
-        removeButton.y += i - y
         super.setY(i)
     }
 
