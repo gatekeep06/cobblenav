7e64f52ee246501a34df30bc0ef078cb19ef0323
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2026-04-18 22:13:53 +0300

    change simple mode screen structure, implement clear method

--- common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/pokefinder/SimplePokefinderScreen.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/gui/screen/pokefinder/SimplePokefinderScreen.kt
@@ -1,5 +1,6 @@
 package com.metacontent.cobblenav.client.gui.screen.pokefinder
 
+import com.metacontent.cobblenav.client.gui.widget.layout.TableView
 import com.metacontent.cobblenav.client.gui.widget.pokefinder.FilterEntryWidget
 import com.metacontent.cobblenav.client.settings.pokefinder.RadarFilterTypeRegistry
 import com.metacontent.cobblenav.client.settings.pokefinder.filter.RadarFilter
@@ -8,14 +9,32 @@ import net.minecraft.client.gui.components.AbstractWidget
 import net.minecraft.network.chat.Component
 
 class SimplePokefinderScreen : AbstractModePokefinderScreen() {
+    private lateinit var filterTable: TableView<FilterEntryWidget>
+
     override fun populateBaseTable(consumer: (AbstractWidget) -> Unit) {
-        settings?.getFilters()?.forEach {
-            RadarFilterTypeRegistry.get(it)?.let { type -> consumer(createEntry(type, it)) }
-        }
+        filterTable = TableView(
+            x = 0,
+            y = 0,
+            width = 238,
+            columns = 1,
+            verticalGap = 6f,
+            horizontalGap = 0f
+        )
+        populateFilterTable()
+        consumer(filterTable)
     }
 
     override fun clearFilters() {
-        settings?.getFilters()?.forEach(RadarFilter::clear)
+        filterTable.applyToAll { it.filter.clear() }
+        filterTable.clear()
+        populateFilterTable()
+    }
+
+    fun populateFilterTable() {
+        val entries = settings?.getFilters()?.mapNotNull {
+            RadarFilterTypeRegistry.get(it)?.let { type -> createEntry(type, it) }
+        } ?: emptyList()
+        filterTable.add(entries)
     }
 
     override fun checkBottomText(): Component? {
