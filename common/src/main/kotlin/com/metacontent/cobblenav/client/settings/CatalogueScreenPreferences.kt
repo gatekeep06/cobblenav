b51d16698d37dc23eff9fb957f0d822e1a543fd9
GateKeep <115021625+gatekeep06@users.noreply.github.com>
2026-05-13 20:38:30 +0300

    add names for the sorting target types

--- common/src/main/kotlin/com/metacontent/cobblenav/client/settings/CatalogueScreenPreferences.kt
+++ common/src/main/kotlin/com/metacontent/cobblenav/client/settings/CatalogueScreenPreferences.kt
@@ -1,8 +1,15 @@
 package com.metacontent.cobblenav.client.settings
 
+import com.metacontent.cobblenav.client.gui.util.translate
+import net.minecraft.network.chat.MutableComponent
+
 data class CatalogueScreenPreferences(
     val sortingTarget: CatalogueSortingTarget,
     val sorting: Sorting
 )
 
-enum class CatalogueSortingTarget { NAME, RESULT_TYPE, BUCKET }
+enum class CatalogueSortingTarget(val displayName: MutableComponent) {
+    NAME(translate("gui.cobblenav.sorting.name")),
+    RESULT_TYPE(translate("gui.cobblenav.sorting.result_type")),
+    BUCKET(translate("gui.cobblenav.sorting.bucket"))
+}
